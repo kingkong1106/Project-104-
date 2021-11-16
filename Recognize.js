d14dc746f1019d518a7d39c36076058ad901545a
kingkong1106 <78448474+kingkong1106@users.noreply.github.com>
2021-11-15 22:46:40 -0500

    Add files via upload

---
+++ Recognize.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality: 90
+});
+var camera = document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snap_shot()
+{
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = '<img id="capture_image" src="' + data_uri + '">';
+    });
+}
+console.log('ml5 version:' , ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/wstQvVs63/',modelLoaded);
+function modelLoaded()
+{
+    console.log('model loaded')
+}
